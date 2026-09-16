a3496bbfe450720b0473e4a829296115a1aa8cf9
dowahh <doawahyudi81@gmail.com>
2026-09-16 17:26:37 +0700

    update

--- src/component/Cardgrid.jsx
+++ src/component/Cardgrid.jsx
@@ -23,7 +23,7 @@ function CardGrid({features}){
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 border-2 border-slate-400">
             {
-                features.map((data) => {
+                dataProduct.map((data) => {
                     return (
                         <Card key={data.id} icon={data.icon} title={data.title} subtitle={data.subtitle}/>
                     )
